655eed1a1f71d55903897004cc4c120755754ff0
jedijk <jedijk@gmail.com>
2026-03-12 20:56:10 +0100

    Create price.js

---
+++ api/price.js
@@ -0,0 +1,26 @@
+export default async function handler(req, res) {
+
+  const fetchPrice = async (symbol) => {
+    const url = `https://query1.finance.yahoo.com/v7/finance/quote?symbols=${symbol}`;
+    const r = await fetch(url);
+    const data = await r.json();
+    return data.quoteResponse.result[0].regularMarketPrice;
+  };
+
+  try {
+
+    const vix = await fetchPrice("^VIX");
+    const brent = await fetchPrice("BZ=F");
+
+    res.status(200).json({
+      vix,
+      brent
+    });
+
+  } catch (error) {
+
+    res.status(500).json({ error: "failed" });
+
+  }
+
+}

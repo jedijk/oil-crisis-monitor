9b17fc80e464a3e9b8e661b1602a9d79c00c47cd
jedijk <jedijk@gmail.com>
2026-03-12 21:04:44 +0100

    Update price.js

--- api/price.js
+++ api/price.js
@@ -1,26 +1,26 @@
-export default async function handler(req, res) {
-
-  const fetchPrice = async (symbol) => {
-    const url = `https://query1.finance.yahoo.com/v7/finance/quote?symbols=${symbol}`;
-    const r = await fetch(url);
-    const data = await r.json();
-    return data.quoteResponse.result[0].regularMarketPrice;
-  };
+export default async function handler(request, response) {
 
   try {
+
+    const fetchPrice = async (symbol) => {
+      const url = `https://query1.finance.yahoo.com/v7/finance/quote?symbols=${symbol}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      return data.quoteResponse.result[0].regularMarketPrice;
+    };
 
     const vix = await fetchPrice("^VIX");
     const brent = await fetchPrice("BZ=F");
 
-    res.status(200).json({
-      vix,
-      brent
+    response.status(200).json({
+      vix: vix,
+      brent: brent
     });
 
   } catch (error) {
 
-    res.status(500).json({
-      error: "Failed to fetch data"
+    response.status(500).json({
+      error: "Failed to fetch market data"
     });
 
   }
